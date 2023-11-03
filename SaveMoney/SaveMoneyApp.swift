dd4293bf3b518857c64ad7752495b404bb4d1863
Trinh Long <longtv@mor.com.vn>
2023-11-03 15:43:15 +0700

    Initial Commit

---
+++ SaveMoney/SaveMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveMoneyApp.swift
+//  SaveMoney
+//
+//  Created by Trinh Long on 03/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SaveMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
